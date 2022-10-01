d9c52ed4cd5404cc719a63738878fda291eaac14
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-01 23:13:36 +0100

    responsiveness tweaks

--- src/styles.tsx
+++ src/styles.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const Root = styled.div`
-  max-width: 1024px;
+  width: 98vw;
   min-height: 98vh;
   display: flex;
   padding: 1em;
@@ -9,4 +9,8 @@ export const Root = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+
+  @media (min-width: 1024px) {
+    max-width: 1024px;
+  }
 `;
