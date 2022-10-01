4bdaa532c402472618f599a6280b419390dee585
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-01 23:01:40 +0100

    update root to use Avatar

--- src/root.tsx
+++ src/root.tsx
@@ -1,11 +1,14 @@
+import avatarImg from './assets/avatar.jpg';
+import * as S from './styles';
 import SocialLinksSection from './sections/social-links';
 import CodingAroundTheWorldSection from './sections/coding-round-the-world';
-import * as S from './styles';
 import JustifiedParagraph from './components/justified-text';
+import Avatar from './components/avatar';
 
 const Root = () => {
   return (
     <S.Root>
+      <Avatar imgSrc={avatarImg} />
       <h1>Kaio Silveira</h1>
       <JustifiedParagraph>
         Father &amp; husband, software engineer, restless student. Loves to solve difficult problems
