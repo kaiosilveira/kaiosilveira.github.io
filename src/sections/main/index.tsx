2723ea4c1716a42c4da854493408beb356dd4e86
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-02 08:59:55 +0100

    add location pin to main section

--- src/sections/main/index.tsx
+++ src/sections/main/index.tsx
@@ -2,7 +2,9 @@ import SocialLinksSection from '../social-links';
 import avatarImg from '../../assets/avatar.jpg';
 import Avatar from '../../components/avatar';
 import JustifiedParagraph from '../../components/justified-text';
+import { faMapMarker } from '@fortawesome/free-solid-svg-icons';
 import * as S from './styles';
+import LabeledIcon from '../../components/labeled-icon';
 
 const MainSection = () => (
   <S.Main>
@@ -12,6 +14,7 @@ const MainSection = () => (
       Father &amp; husband, software engineer, restless student. Loves to solve difficult problems
       and to talk about philosophical stuff with his 5yo boy.
     </JustifiedParagraph>
+    <LabeledIcon icon={faMapMarker} label="Lisbon, Portugal" />
     <SocialLinksSection />
   </S.Main>
 );
