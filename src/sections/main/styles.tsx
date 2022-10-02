5aa8b6761248dcefd2e9ac8f2d0b16b162143b65
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-02 10:27:35 +0100

    justify content with space-around at main section

--- src/sections/main/styles.tsx
+++ src/sections/main/styles.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Main = styled.main`
   display: flex;
   flex-direction: column;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
   width: 98vw;
   height: 98vh;
