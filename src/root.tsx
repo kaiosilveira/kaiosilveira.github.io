b5c43d4f442a65abcf8eeb0fa758e6c0c4e4d104
Kaio Silveira <silveira.kaio@icloud.com>
2022-10-02 10:21:32 +0100

    add who-am-i section

--- src/root.tsx
+++ src/root.tsx
@@ -1,11 +1,13 @@
 import * as S from './styles';
 import MainSection from './sections/main';
 import CodingAroundTheWorldSection from './sections/coding-round-the-world';
+import WhoAmISection from './sections/who-am-i';
 
 const Root = () => {
   return (
     <S.Root>
       <MainSection />
+      <WhoAmISection />
       <CodingAroundTheWorldSection />
     </S.Root>
   );
---
+++ src/sections/who-am-i/index.tsx
@@ -0,0 +1,19 @@
+import JustifiedParagraph from '../../components/justified-text';
+
+const WhoAmISection = () => (
+  <section>
+    <h2>Who am I? 🥷🏽</h2>
+    <JustifiedParagraph>
+      I'm a person who occasionally started writing code around seven years ago and since then
+      became obsessed with it. I like playing games with my son (who aims to be a gamer when he
+      grows up) and walking around the city to think about life and all the things. Professionally,
+      I've worked for many different companies in many different domains and acquired a lot of
+      technical knowledge out of that, specially in web development. I care a lot about software
+      architecture and code quality, that's why I'm always trying to improve my own assumptions and
+      to solidify more and more my knowledge base by reading tech books and staying tuned with
+      newly-released, up and coming technologies.
+    </JustifiedParagraph>
+  </section>
+);
+
+export default WhoAmISection;
