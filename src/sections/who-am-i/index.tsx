b02273cc1a7218c625c490a3a25c88a021c06761
Kaio Silveira <silveira.kaio@icloud.com>
2023-03-09 16:32:25 +0000

    update who am I section

--- src/sections/who-am-i/index.tsx
+++ src/sections/who-am-i/index.tsx
@@ -4,14 +4,7 @@ const WhoAmISection = () => (
   <section>
     <h2>Who am I? 🥷🏽</h2>
     <JustifiedParagraph>
-      I'm a person who occasionally started writing code around seven years ago and since then
-      became obsessed with it. I like playing games with my son (who aims to be a gamer when he
-      grows up) and walking around the city to think about life and all the things. Professionally,
-      I've worked for many different companies in many different domains and acquired a lot of
-      technical knowledge out of that, specially in web development. I care a lot about software
-      architecture and code quality, that's why I'm always trying to improve my own assumptions and
-      to solidify more and more my knowledge base by reading tech books and staying tuned with
-      newly-released, up and coming technologies.
+      I'm a person who occasionally started writing code around eight years ago and since then became obsessed with it. I like playing games with my son (who aims to be a gamer when he grows up) and walking around the city to think about life and all things. Professionally, I've worked for many different companies in many different domains and acquired a lot of technical knowledge from that, especially in web development. I care a lot about software architecture and code quality, that's why I'm always trying to improve my own assumptions and solidify more and more my knowledge base by reading tech books and staying tuned with newly-released, up-and-coming technologies.
     </JustifiedParagraph>
   </section>
 );
